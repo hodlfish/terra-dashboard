372e29a5e336d6d58f21baf44af93544b971a82b
Theo Dubiel <theodore.dubiel@gmail.com>
2022-03-31 16:30:48 -0500

    Added missing chart.js import.

--- src/scripts/Graphing.ts
+++ src/scripts/Graphing.ts
@@ -1,6 +1,6 @@
-import {Chart as ChartJS, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Filler, defaults, Tooltip, BarElement} from 'chart.js'
+import {Chart as ChartJS, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Filler, defaults, Tooltip, BarElement, BarController} from 'chart.js'
 
 export default function InitializeChartJS() {
-    ChartJS.register(ArcElement, CategoryScale, LinearScale, PointElement, LineElement, BarElement, Filler, Tooltip);
+    ChartJS.register(ArcElement, CategoryScale, LinearScale, PointElement, LineElement, BarElement, BarController, Filler, Tooltip);
     defaults.animation = false;
 }
